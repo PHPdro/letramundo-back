6c1f743ff80c451c6c187aac97be14cd1a85a6da
PHPdro <pedro110302@gmail.com>
2025-04-23 21:29:35 -0300

    fix seeders again

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
         User::create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
         ]);
 
         $themes = [
