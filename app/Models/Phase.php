ab45e46c184436579646f9a66feec2211d689969
Arthur-4 <arthur290402@gmail.com>
2025-06-19 03:06:56 -0300

    feat: add test for retrieving a student after creation

--- app/Models/Phase.php
+++ app/Models/Phase.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Phase extends Model
 {
+
+    protected $fillable = [
+        'phase',
+        'level_id',
+    ];
+    
     public function level () : BelongsTo
     {
         return $this->belongsTo(Level::class);
